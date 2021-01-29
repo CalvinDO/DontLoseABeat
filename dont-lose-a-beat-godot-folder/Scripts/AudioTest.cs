b3bfa89d0ce101dc83b379ad85a2fdbe2408a906
Calvin <calvindelloro@mail.de>
2021-01-29 12:28:03 +0100

    scene cleanup

--- dont-lose-a-beat-godot-folder/Scripts/AudioTest.cs
+++ dont-lose-a-beat-godot-folder/Scripts/AudioTest.cs
@@ -21,18 +21,20 @@ public class AudioTest : AudioStreamPlayer
 
     private void SetPitch(float pitch)
     {
-        /*
+
         this.pianoPitch.PitchScale = pitch / this.currentSpeed;
         this.PitchScale = pitch;
         this.currentSpeed = pitch;
-        */
-        this.pianoPitch.PitchScale = 0.5f;
     }
 
+    public void SetPitchAndSpeed(float pitch, float speed)
+    {
+        this.SetPitch(pitch);
+        this.SetSpeed(speed);
+    }
     public override void _Ready()
     {
-        this.SetPitch(1.5f);
-       // this.SetSpeed(0.8f);
+        this.SetPitchAndSpeed(1.5f, 0.8f);
     }
 
 
