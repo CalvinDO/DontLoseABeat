5ddfabfdef25234f1d0c0a12b596abb4ea359349
Calvin <calvindelloro@mail.de>
2021-01-31 14:17:45 +0100

    asf

--- dont-lose-a-beat-godot-folder/Scripts/IntroSwitch.cs
+++ dont-lose-a-beat-godot-folder/Scripts/IntroSwitch.cs
@@ -8,11 +8,7 @@ public class IntroSwitch : Spatial
 
     public override void _Ready() {
         GameState.mainScene = GD.Load<PackedScene>("res://Scenes/MainScene.tscn");
-<<<<<<< HEAD
-
-=======
         GameState.transitionScene = GD.Load<PackedScene>("res://Scenes/leveltransition.tscn");
->>>>>>> d76f01d5c650ac5b8dc93ec4eb5964e73c1b402b
     }
     public void LoadMainScene()
     {
