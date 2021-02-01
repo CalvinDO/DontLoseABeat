60cbf95bc3cda6925ad4597a00dec98aba02a72e
SimonStorlSchulke <simonstoschu@googlemail.com>
2021-02-02 00:42:43 +0100

    Fix Horns

--- dont-lose-a-beat-godot-folder/Scripts/Selector.cs
+++ dont-lose-a-beat-godot-folder/Scripts/Selector.cs
@@ -6,18 +6,20 @@ public class Selector : RayCast {
     public override void _Input(InputEvent eInput) {
 
         if (eInput.IsActionPressed("InstrumentSelect") && IsColliding()) {
-            try {
+            //try {
 
                 //todo add exceptionhandling
                 if (GameState.selectedSection != null) GameState.selectedSection.ToggleRightLeftAreas(false);
 
                 Area viewedArea = (Area)GetCollider();
+                GD.Print($"ar {viewedArea}");
                 PlayerSection viewedSection = (PlayerSection)viewedArea.GetParent().GetParent();
+                GD.Print($"se {viewedSection}");
                 GameState.selectedSection = viewedSection;
                 GameState.selectedSection.ToggleRightLeftAreas(true);
-            } catch {
-                GD.Print($"input {eInput}");
-            }
+            //} catch {
+               // GD.Print($"input {eInput}");
+           // }
         }
     }
 }
